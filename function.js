e55f99e2dc66157a19b595ca5166239ab2e2c5c3
Kensuke Kameyama <hb18a033@oecu.jp>
2019-11-19 16:33:57 +0900

    ボタンのバグを修正しました。

--- function.js
+++ function.js
@@ -2,6 +2,6 @@ function button1(){														//水族館のボタン
 	alert("そんなものは存在しません");
 }
 
-function button1(){														//動物園のボタン
+function button2(){														//動物園のボタン
 	alert("存在してたまるかそんなもの");
 }
